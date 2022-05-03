5811cc5e4f5cad7e10567e2110c9f967c36ba718
kabirulpHero <kabirulislamuo@gmail.com>
2022-05-04 02:24:10 +0600

    get set env setup for firebase

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import About from './Pages/About/About';
 import Home from './Pages/Home/Home/Home';
 import Service from './Pages/Home/Service/Service';
 import Login from './Pages/Login/Login';
+import Register from './Pages/Register/Register';
 import ServiceDetail from './Pages/ServiceDetail/ServiceDetail';
 import Footer from './Pages/Shared/Footer/Footer';
 import Header from './Pages/Shared/Header/Header';
@@ -19,6 +20,7 @@ function App() {
         <Route path='/service/:serviceId'element={<ServiceDetail></ServiceDetail>}></Route>
         <Route path='/about' element={<About></About>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
+        <Route path='/register' element={<Register></Register>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -6,12 +6,12 @@ import {getAuth} from './firebase/auth';
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBAxKaLUSpE-YKmxSHRCwsb7btDbVCqmFQ",
-  authDomain: "genius-car-services-75859.firebaseapp.com",
-  projectId: "genius-car-services-75859",
-  storageBucket: "genius-car-services-75859.appspot.com",
-  messagingSenderId: "752223459925",
-  appId: "1:752223459925:web:481c3d59f11c9b77b092dc"
+  apiKey:process.env.REACT_APP_apiKey,
+  authDomain:process.env.REACT_APP_authDomain,
+  projectId:process.env.REACT_APP_projectId,
+  storageBucket:process.env.REACT_APP_storageBucket,
+  messagingSenderId:process.env.REACT_APP_messagingSenderId,
+  appId:process.env.REACT_APP_appId,
 };
 
 // Initialize Firebase
